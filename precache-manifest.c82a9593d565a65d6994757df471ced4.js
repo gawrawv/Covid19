41c58b102bbe443f7b77208faa5b48a8630b1d13
unknown <young.gaurav123@gmail.com>
2021-04-19 22:18:41 +0545

    Updates

---
+++ precache-manifest.c82a9593d565a65d6994757df471ced4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3874c00bc06495e7d04ed9cd8e05966",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "661ef310435dae3c1cfd",
+    "url": "/covid-19/static/css/main.1f4c035c.chunk.css"
+  },
+  {
+    "revision": "b79e184e8be111b87801",
+    "url": "/covid-19/static/js/2.6a82466f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.6a82466f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661ef310435dae3c1cfd",
+    "url": "/covid-19/static/js/main.473832df.chunk.js"
+  },
+  {
+    "revision": "b3a3bb7da73e882ef60e",
+    "url": "/covid-19/static/js/runtime-main.aeb58cd9.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covid-19/static/media/covid-19.6ce9074d.png"
+  }
+]);
